e89d199ac079758b940db947d2a50d9b7bffbfc3
Rou <caubesieunhan345@gmail.com>
2022-09-18 16:43:12 +0700

    add starter code and bomber movement

---
+++ src/uet/oop/bomberman/Const.java
@@ -0,0 +1,5 @@
+package uet.oop.bomberman;
+
+public class Const {
+    public static int MOVINGSPEED = 1;
+}
---
+++ src/uet/oop/bomberman/input/Keyboard.java
@@ -0,0 +1,26 @@
+package uet.oop.bomberman.input;
+
+
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Keyboard implements EventHandler<KeyEvent> {
+    private Set<KeyCode> activeKeys = new HashSet<>();
+
+    @Override
+    public void handle(KeyEvent event) {
+        if (KeyEvent.KEY_PRESSED.equals(event.getEventType())) {
+            activeKeys.add(event.getCode());
+        } else if (KeyEvent.KEY_RELEASED.equals(event.getEventType())) {
+            activeKeys.remove(event.getCode());
+        }
+    }
+
+    public Set<KeyCode> getActiveKeys() {
+        return activeKeys;
+    }
+}
